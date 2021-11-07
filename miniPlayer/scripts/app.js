4b7566f586d53234230f703fb097d224f517b021
Benyamin Fallahi Motlagh <a360pilot@gmail.com>
2021-11-07 12:14:28 +0330

    Updated Files for Version 1.1

--- miniPlayer/scripts/app.js
+++ miniPlayer/scripts/app.js
@@ -3,9 +3,15 @@ const play = document.querySelector('#play');
 const elapsed = document.querySelector('.elapsed');
 const slider = document.querySelector('.slider');
 const timer = document.querySelector('.timer');
+const like = document.querySelector('#like');
+const vol = document.querySelector('#vol-slider');
+
+const volUp = document.querySelector('#vol-up');
+const volDown = document.querySelector('#vol-down');
 
 let duration = 0;
 let isPlaying = false;
+music.volume = vol.value;
 
 music.loop = false;
 music.addEventListener('canplaythrough', function() {
@@ -15,6 +21,30 @@ music.addEventListener('canplaythrough', function() {
 
 music.addEventListener('timeupdate', timeUpdate);
 play.addEventListener('click', playPause);
+
+volUp.addEventListener('click', function() {
+    changeVolume(0.1);
+});
+volDown.addEventListener('click', function() {
+    changeVolume(-0.1);
+});
+
+vol.addEventListener('wheel', function(evObj) {
+    if(evObj.wheelDeltaY > 0) {
+        changeVolume(0.1);
+    } else {
+        changeVolume(-0.1);
+    }
+    return;
+}, {passive: true});
+vol.addEventListener('input', function() {
+    vol.style.backgroundSize = `${vol.value * 100}% 100%`;
+})
+
+slider.addEventListener('click', function(evObj) {
+    let clickTime = (slider.offsetWidth - (evObj.layerX - slider.offsetLeft)) / slider.offsetWidth * duration;
+    music.currentTime = clickTime;
+})
 
 function timeUpdate() {
     let playPercent = slider.offsetWidth * (music.currentTime / duration);
@@ -29,6 +59,10 @@ function timeUpdate() {
     } else {
         timer.innerHTML = toFarsiNumber(minutes) + ":" + toFarsiNumber(seconds);
     }
+
+    // Check also for volume changes:
+    music.volume = vol.value;
+
     return;
 }
 
@@ -59,7 +93,7 @@ function playPause() {
 }
 
 // Event listeners for keyboard shortcuts:
-document.addEventListener('keyup', function(evObj) {
+document.addEventListener('keydown', function(evObj) {
     key = evObj.key;
 
     if(key === 'k') {
@@ -70,4 +104,29 @@ document.addEventListener('keyup', function(evObj) {
         music.muted = !music.muted;
         return;
     }
+    if(key === 'ArrowUp') {
+        changeVolume(0.1);
+        return;
+    }
+    if(key === 'ArrowDown') {
+        changeVolume(-0.1);
+        return;
+    }
 });
+
+function changeVolume(value) {
+    // Check overflows:
+    if((value + music.volume > 1)
+                ||
+       (value + music.volume < 0)) {
+           music.volume = (value > 0) ? 1 : 0;
+           vol.value = music.volume;
+           vol.style.backgroundSize = `${vol.value * 100}% 100%`
+           return;
+    }
+
+    music.volume = music.volume + value;
+    vol.value = music.volume;
+    vol.style.backgroundSize = `${vol.value * 100}% 100%`
+    return;
+}
